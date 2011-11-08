9844449446565e61994ec1482467b5c122b220b5
Sean Swezey <sean.swezey@gmail.com>
2011-11-08 14:36:15 -0500

    Initial commit

---
+++ src/edu/gatech/smartfridge/SmartFridgeAndroidActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.smartfridge;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SmartFridgeAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
